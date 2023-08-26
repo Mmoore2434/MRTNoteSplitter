0a1122ab476090ead0d4e69f529180b2b468d7ce
Madmmoore <Mmoore2434@gmail.com>
2023-08-25 21:21:16 -0400

    changed port, maybe it does something

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ wss.on('connection', function (ws) {
   });
 });
 
-server.listen(8080, function () {
-  console.log('Listening on http://0.0.0.0:8080');
+server.listen(3000, function () {
+  console.log('Listening on http://0.0.0.0:3000');
 });
